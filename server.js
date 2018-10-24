a414f0a9462a58880980d92606aeed3bad2e6a03
Julian Villarreal <mr.v@Julians-Air.attlocal.net>
2018-10-23 21:37:06 -0500

    working server

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.use(bodyParser.json());
 
 // ================================================================================
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-// ================================================================================
+
 
 // require("./app/routes/apiRoutes")(app);
 require("./app/routes/html_routes.js")(app);
